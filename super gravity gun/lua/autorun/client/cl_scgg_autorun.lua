75605147f586e58062cbe0f56debf85cee0cff92
Shadowysn <44493487+Shadowysn@users.noreply.github.com>
2019-12-08 17:33:43 +0800

    Updated to new version

---
+++ super gravity gun/lua/autorun/client/cl_scgg_autorun.lua
@@ -0,0 +1,114 @@
+if (SERVER) then return end
+
+CreateClientConVar( "cl_scgg_viewmodel", "models/weapons/shadowysn/c_superphyscannon.mdl", true, true, 
+	"Set the viewmodel of your Super Gravity Gun. Does not affect worldmodel." )
+
+CreateClientConVar( "cl_scgg_physgun_color", "0", true, true, 
+	"Set the glow color to your physgun's color in first-person. Third-person is not affected." )
+
+CreateClientConVar( "cl_scgg_effects_mode", "0", true, true, 
+	"Set the effect style to emulate from a game. Third-person is not affected. 0 = Half-Life 2, 1 = Half-Life 2 Survivor" )
+
+net.Receive("SCGG_Core_Muzzle", function()
+	local core = net.ReadEntity()
+	if IsValid(core) then
+		core.Muzzle = true
+		timer.Simple( 0.12, function() 
+			if IsValid(core) then
+			core.Muzzle = nil
+			end
+		end)
+	end
+end)
+
+hook.Add("Think", "Grav_Disable_Claw_Bug", function() 
+	local grav = LocalPlayer():GetWeapon("weapon_physcannon")
+	if IsValid(grav) then
+		if IsValid(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass() == "weapon_physcannon" then
+		grav:SetNextClientThink(CurTime())
+		else
+		grav:SetNextClientThink(CurTime() + 0.2)
+		end
+	end
+end)
+-- ^^ Rubat still didn't apparently fix it. :|
+	
+if cvars.GetConVarCallbacks("cl_scgg_viewmodel", false) != nil then
+	cvars.RemoveChangeCallback("cl_scgg_viewmodel", "scgg_viewmodel_cvar_checker")
+end
+cvars.AddChangeCallback("cl_scgg_viewmodel", function(cvar, old, new) 
+	if !util.IsValidModel(new) or IsUselessModel(new) then
+		--util.PrecacheModel(new)
+		GetConVar("cl_scgg_viewmodel"):SetString("models/weapons/shadowysn/c_superphyscannon.mdl")
+		LocalPlayer():PrintMessage( HUD_PRINTCONSOLE, "Model is not valid. If it exists, make sure it is precached serverside first." )
+	end
+end, "scgg_viewmodel_cvar_checker")
+
+local function HL2Options(panel) -- HL2 Options for the menu.
+local HL2Options = {Options={},
+CVars={},
+Label="#Presets",
+MenuButton="1",
+Folder="options"}
+panel:ControlHelp("")
+panel:AddControl("Label", {Text = "The Super Gravity Gun is found under Weapons"})
+panel:ControlHelp("Weapons")
+panel:ControlHelp("Half-Life 2")
+panel:AddControl("Label", {Text = "It can also be spawned under"})
+panel:ControlHelp("Entities")
+panel:ControlHelp("Half-Life 2")
+panel:AddControl("Label", {Text = "Credits:"})
+panel:ControlHelp("Î¤yler Blu  - Original Super Gravity Gun")
+panel:ControlHelp("ErrolLiamP - Fixing / Porting and Additions")
+HL2Options.Options["#Default"]={scgg_enabled="1", scgg_style="0", scgg_friendly_fire="1", scgg_weapon_vaporize="0", scgg_allow_others="0", scgg_keep_armor="0", scgg_claw_mode="1", scgg_light="0", scgg_muzzle_flash="1", scgg_zap="1", scgg_zap_sound="1", scgg_no_effects="0", scgg_equip_sound="0"}
+panel:AddControl("ComboBox",HL2Options)
+panel:AddControl("Slider",{Label = "Weapon Status",min = 0,max = 2,Command = "scgg_enabled"})--1
+panel:ControlHelp("0 = The weapon will be disabled")
+panel:ControlHelp("1 = The weapon will be enabled")
+panel:ControlHelp("2 = The weapon will be enabled, with other changes to the map")
+panel:AddControl("Slider",{Label = "Behavior",min = 0,max = 1,Command = "scgg_style"})--2
+--panel:ControlHelp("")
+panel:ControlHelp("0 = Half-Life 2 Styled - Slower and Weaker")
+panel:ControlHelp("1 = Garry's Mod Styled - Faster and Stronger")
+panel:AddControl("Slider",{Label = "Friendly Fire (NPC)",min = 0,max = 1,Command = "scgg_friendly_fire"})--3
+panel:ControlHelp("0 = Friendly NPCs will not be directly targeted")
+panel:ControlHelp("1 = Friendly NPCs will be directly targeted")
+panel:AddControl("Slider",{Label = "Weapon Vaporization",min = 0,max = 1,Command = "scgg_weapon_vaporize"})--4
+panel:ControlHelp("0 = Disabled")
+panel:ControlHelp("1 = Dropped weapons will be vaporized map-wide")
+panel:AddControl("Slider",{Label = "Foreign Interaction",min = 0,max = 1,Command = "scgg_allow_others"})--5
+panel:ControlHelp("0 = The weapon will not interact with foreign objects")
+panel:ControlHelp("1 = The weapon will interact with foreign objects")
+panel:AddControl("Label", {Text = "Foreign Interaction can cause bugs! Use at your own risk."})
+panel:AddControl("Slider",{Label = "Armor Drain",min = 0,max = 2,Command = "scgg_keep_armor"})--6
+panel:ControlHelp("0 = All armor will be depleted on weapon disable")
+panel:ControlHelp("1 = Armor will be depleted to 100% on weapon disable")
+panel:AddControl("Slider",{Label = "Claw Behavior",min = 0,max = 2,Command = "scgg_claw_mode"})--7
+panel:ControlHelp("0 = Claws always closed")
+panel:ControlHelp("1 = Claws always open")
+panel:ControlHelp("2 = Claws in dynamic state")
+panel:AddControl("Slider",{Label = "Light Settings",min = 0,max = 1,Command = "scgg_light"})--8
+panel:ControlHelp("0 = The weapon will not emit a light")
+panel:ControlHelp("1 = The weapon will emit a light")
+panel:AddControl("Slider",{Label = "Muzzle Flash Settings",min = 0,max = 1,Command = "scgg_muzzle_flash"})--9
+panel:ControlHelp("0 = The weapon will not emit a light")
+panel:ControlHelp("1 = The weapon will emit a light")
+panel:AddControl("Slider",{Label = "Electrocute Victims",min = 0,max = 1,Command = "scgg_zap"})--10
+panel:ControlHelp("0 = The victim will not be electrocuted")
+panel:ControlHelp("1 = The victim will be electrocuted")
+panel:AddControl("Slider",{Label = "Electrocuted Sounds",min = 0,max = 1,Command = "scgg_zap_sound"})--11
+panel:ControlHelp("0 = Electrocuted victims will not emit sounds")
+panel:ControlHelp("1 = Electrocuted victims will emit sounds")
+panel:AddControl("Slider",{Label = "Visual Effects",min = 0,max = 1,Command = "scgg_no_effects"})--12
+panel:ControlHelp("0 = Visual Effects enabled")
+panel:ControlHelp("1 = Visual Effects disabled")
+panel:ControlHelp("(Holster and Deploy weapon to take effect.)")
+panel:AddControl("Slider",{Label = "Equipping Sound",min = 0,max = 1,Command = "scgg_equip_sound"})--13
+panel:ControlHelp("0 = The weapon will not emit a charge sound after deploying")
+panel:ControlHelp("1 = The weapon will emit a charge sound after deploying")
+end
+
+function HL2AddOption()
+spawnmenu.AddToolMenuOption("Utilities", "Half-Life 2", "Main Settings - HL2", "Info / Settings", "", "", HL2Options)
+end
+hook.Add("PopulateToolMenu", "HL2AddOption", HL2AddOption)
---
+++ super gravity gun/lua/autorun/server/sv_scgg_autorun.lua
@@ -0,0 +1,393 @@
+if (CLIENT) then return end
+
+util.AddNetworkString("SCGG_Ragdoll_GetPlayerColor")
+util.AddNetworkString("SCGG_Core_Muzzle")
+
+local function DissolveEntity(entity)
+	local name = "SCGG_Dissolving_"..entity:EntIndex()
+	entity:SetName( name )
+	--[[if !IsValid(dissolver) then
+	local dissolver = ents.Create("env_entity_dissolver")
+	dissolver:SetPos(Vector(0,0,0))
+	dissolver:SetKeyValue( "dissolvetype", 0 )
+	dissolver:SetName("scgg_addon_global_dissolver")
+	dissolver:Spawn()
+	dissolver:Activate()
+	end
+	dissolver:Fire("Dissolve", name, 0)--]]
+	local check_bool = nil
+	for _,ent in pairs(ents.FindByName("scgg_addon_global_dissolver")) do
+		if ent:GetClass() == "env_entity_dissolver" then
+			ent:Fire("Dissolve", name, 0)
+			check_bool = true
+		end
+	end
+	if !check_bool then
+	local dissolver = ents.Create("env_entity_dissolver")
+	dissolver:SetPos(Vector(0,0,0))
+	dissolver:SetKeyValue( "dissolvetype", 0 )
+	dissolver:SetName("scgg_addon_global_dissolver")
+	dissolver:Spawn()
+	dissolver:Activate()
+	dissolver:Fire("Dissolve", name, 0)
+	end
+end
+
+local phys_string = "weapon_physcannon"
+local superphys_string = "weapon_superphyscannon"
+
+if !ConVarExists("scgg_style") then	
+   CreateConVar("scgg_style", '0', (FCVAR_ARCHIVE), "to change if the weapon is styled like Half-Life 2 or Garry's Mod.", 0, 1)
+end--1
+
+if !ConVarExists("scgg_light") then	
+   CreateConVar("scgg_light", '0', (FCVAR_ARCHIVE), "to change if the weapon emits a light.", 0, 1)
+end--2
+
+if !ConVarExists("scgg_muzzle_flash") then	
+   CreateConVar("scgg_muzzle_flash", '1', (FCVAR_ARCHIVE), "to change if the weapon emits a light when attacking.", 0, 1)
+end--3
+
+if !ConVarExists("scgg_zap") then	
+   CreateConVar("scgg_zap", '1', (FCVAR_ARCHIVE), "to toggle victims being electrocuted.", 0, 1)
+end--4
+
+if !ConVarExists("scgg_allow_others") then	
+   CreateConVar("scgg_allow_others", '0', (FCVAR_ARCHIVE), 
+   "to allow weapon interaction of other objects, including addons. (WILL have a chance to cause bugs.)", 
+   0, 1)
+end--5
+
+if !ConVarExists("scgg_zap_sound") then	
+   CreateConVar("scgg_zap_sound", '1', (FCVAR_ARCHIVE), "to toggle electrocuted victims emitting sound.", 0, 1)
+end--6
+
+if !ConVarExists("scgg_equip_sound") then	
+   CreateConVar("scgg_equip_sound", '0', (FCVAR_ARCHIVE), "to toggle sound emitted when deploying weapon.", 0, 1)
+end--7
+
+if !ConVarExists("scgg_no_effects") then	
+   CreateConVar("scgg_no_effects", '0', (FCVAR_ARCHIVE), "to toggle visual effects.", 0, 1)
+end--8
+
+if !ConVarExists("scgg_enabled") then	
+   CreateConVar("scgg_enabled", '1', (FCVAR_ARCHIVE), 
+   "to toggle weapon availability. 0 = any super-charged gravity gun will revert to normal. 1 = Enable, don't do anything else. 2 = Enable, alter various settings.", 
+   0, 2)
+end--9
+
+if !ConVarExists("scgg_allow_enablecvar_modify") then	
+   CreateConVar("scgg_allow_enablecvar_modify", '1', (FCVAR_ARCHIVE), "to toggle whether the game can modify the status of the weapon.", 0, 1)
+end--10
+
+if !ConVarExists("scgg_cone") then	
+   CreateConVar("scgg_cone", '0', (FCVAR_ARCHIVE), "to enable grabbing objects without directly looking at them, via a cone.", 0, 1)
+end--11
+
+if !ConVarExists("scgg_weapon_vaporize") then	
+   CreateConVar("scgg_weapon_vaporize", '0', (FCVAR_ARCHIVE), "to toggle map-wide dropped weapon vaporization.", 0, 1)
+end--12
+
+if !ConVarExists("scgg_keep_armor") then	
+   CreateConVar("scgg_keep_armor", '0', (FCVAR_ARCHIVE), "to keep armor after weapon disable. 0 = remove all armor. 1 = lower to 100. 2 = keep armor.", 0, 2)
+end--13
+
+if !ConVarExists("scgg_friendly_fire") then	
+   CreateConVar("scgg_friendly_fire", '1', (FCVAR_ARCHIVE), "to toggle direct weapon interaction against friendly NPCs.", 0, 1)
+end--14
+
+if !ConVarExists("scgg_claw_mode") then	
+   CreateConVar("scgg_claw_mode", '1', (FCVAR_ARCHIVE), "to toggle claw movement options. 0 = closed. 1 = open. 2 = dynamic.", 0, 2)
+end--15
+
+if !ConVarExists("scgg_deploy_style") then	
+   CreateConVar("scgg_deploy_style", '1', (FCVAR_ARCHIVE), "to change the deploy speed. Legacy attribute from scgg_style. 0 = HL2 speed. 1 = sv_defaultdeployspeed convar.", 0, 1)
+end--16
+
+--game.SetGlobalState( "super_phys_gun", GLOBAL_ON )
+
+--if SERVER then
+--function TheFunction(client, command, arguments, ply)
+    	--ply:Give(superphys_string)
+	--ply:StripWeapon( phys_string )
+--end
+
+--concommand.Add("physcannon_mega_enabled", TheFuction) end
+
+local GetEnts = ents.GetAll()
+local Has1ChangedModifyCvar = false
+hook.Add("OnEntityCreated","SCGG_Trigger_AddOutput",function( trigger ) 
+--for _,trigger in pairs(GetEnts) do
+	if IsValid(trigger) and trigger:GetClass() == "trigger_weapon_dissolve" then
+		local function EntityGlobalCheck()
+			for _,ent in pairs(GetEnts) do
+			if IsValid(ent) and ent:GetClass() == "env_global" and ent:GetName() == "scgg_addon_global_env_for_weapondissolve" then
+				return true
+			end
+			end
+			return false
+		end
+		if EntityGlobalCheck() == false then
+		local global_entity = ents.Create("env_global")
+		global_entity:SetKeyValue("globalstate", "super_phys_gun")
+		global_entity:SetName("scgg_addon_global_env_for_weapondissolve")
+		global_entity:Spawn()
+		global_entity:Activate()
+		end
+		trigger:Fire("AddOutput", "onchargingphyscannon scgg_addon_global_env_for_weapondissolve,TurnOn")
+		--trigger:Fire("AddOutput", "onchargingphyscannon !activator,AddOutput,spawnflags 0, 4")
+		trigger:Fire("AddOutput", "onchargingphyscannon weapon_physcannon,Skin,1")
+		trigger:Fire("AddOutput", "onchargingphyscannon weapon_physcannon,AddOutput,spawnflags 0, 8")
+	end
+end)
+
+hook.Add("PlayerDroppedWeapon","SCGG_Weapon_CheckDrop",function( owner, wep ) 
+-- ^ Remove the other gravity gun if one is dropped.
+if GetConVar("scgg_enabled"):GetInt() >= 2 then
+	owner.SCGG_Dropping = true
+	
+	if wep:GetClass() == phys_string then
+		if owner:HasWeapon(superphys_string) == true then
+			local physgun = owner:GetWeapon(superphys_string)
+			--[[if owner:GetActiveWeapon() == physgun then -- Unfinished.
+				owner:
+			end--]]
+			physgun:Remove()
+		end
+	end
+	--[[if wep:GetClass() == superphys_string then -- Buggy behaviour resulting in infinite grav guns, both normal and charged.
+		if owner:HasWeapon(phys_string) == true then
+			owner.SCGG_Dropping = true
+			local physgun = owner:GetWeapon(phys_string)
+			--if owner:GetActiveWeapon() == physgun then
+			--	owner:
+			--end
+			physgun:Remove()
+		end
+	end--]]
+	owner.SCGG_Dropping = nil
+end
+end )
+
+hook.Add("Think","SCGG_Global_Think",function() 
+-- ^ Start of think hook
+if (GetConVar("scgg_weapon_vaporize"):GetInt() > 0 and GetConVar("scgg_weapon_vaporize"):GetInt() < 2) then
+-- ^ Start of vaporize cvar check
+
+	for _,wpn in pairs(ents.GetAll()) do
+		wpn.SCGG_Dissolving = false -- Dissolve check.
+		--[[if IsValid(wpn) and wpn:IsWeapon() and !wpn:GetOwner():IsValid() then
+			wpn:SetKeyValue("spawnflags","2") 
+			-- ^ I don't know what I was trying to do with this.
+		end--]]
+			if IsValid(wpn) and wpn:IsValid() and ( wpn:IsWeapon() or wpn:GetClass() == "item_ammo_ar2_altfire" ) and !wpn:CreatedByMap() and 
+			(wpn:GetClass() != phys_string and wpn:GetClass() != superphys_string) then
+			-- ^ Valid check start
+		for _, child in pairs(wpn:GetChildren()) do
+			if child:GetClass() == "env_entity_dissolver" then
+				wpn.SCGG_Dissolving = true 
+				-- ^ Mark them as dissolving so we don't repeatedly try to dissolve an already vaporizing gun.
+			end
+		end
+		
+		if GetConVar("scgg_enabled"):GetInt() >= 2 and wpn:GetClass() == "item_ammo_ar2_altfire" then 
+		-- ^ Check for scgg_enabled cvar num to be 2 and target being AR2 pulse ball ammo.
+		
+		local fakeitem = ents.Create("prop_physics_override") 
+		-- ^ Replace AR2 pulse ball ammo with fake props, as they will still be picked up when vaporizing.
+		fakeitem:SetPos( wpn:GetPos() )
+		fakeitem:SetAngles( wpn:GetAngles() )
+		fakeitem:SetModel( wpn:GetModel() )
+		fakeitem:SetSkin( wpn:GetSkin() )
+		fakeitem:SetColor( wpn:GetColor() )
+		fakeitem:SetCollisionGroup( COLLISION_GROUP_WEAPON )
+		fakeitem:Spawn()
+		fakeitem:Activate()
+		undo.ReplaceEntity( wpn, fakeitem )
+		wpn:Remove()
+		
+		DissolveEntity(fakeitem)
+		elseif !wpn:GetOwner():IsValid() and wpn.SCGG_Dissolving == false then 
+		-- ^ Check if it's not in the hands of an NPC or Player, and not being dissolved.
+		DissolveEntity(wpn)
+		end 
+		-- ^ Dissolve end.
+			end 
+			-- ^ Valid check end
+		--[[if IsValid(wpn) and wpn:IsWeapon() and !wpn:GetOwner():IsValid() and wpn.SCGG_Dissolving == false then
+		wpn:SetKeyValue("spawnflags","0")
+		-- ^ I don't know what I was trying to do with this.
+		end--]]
+	end
+end
+-- ^ End of vaporize cvar check
+if (game.GetGlobalState( "super_phys_gun") == GLOBAL_ON or GetConVar("scgg_enabled"):GetInt() >= 2) then 
+-- ^ Check if global state turned on and cvar is not 1
+	if GetConVar("scgg_allow_enablecvar_modify"):GetInt() > 0 and game.GetGlobalState( "super_phys_gun") == GLOBAL_ON then
+	GetConVar("scgg_enabled"):SetInt(2)
+	end
+	
+	-- v Attempt to make gravity guns glow the physgun color, but went awry. (players were somehow affected)
+	--[[for _,physcannon in pairs(ents.GetAll()) do
+		if physcannon:GetClass(phys_string) then
+		--local supermdl = "models/weapons/errolliamp/w_superphyscannon.mdl"
+		--local mdl = "models/weapons/w_physics.mdl"
+		
+		if !IsValid( physcannon:GetOwner() ) then
+			if physcannon:GetSkin() != 1 then
+			physcannon:SetSkin( 1 )
+			end
+		elseif IsValid( physcannon:GetOwner() ) then
+			if physcannon:GetSkin() != 0 then
+			physcannon:SetSkin( 0 )
+			end
+		end
+		
+		end
+	end --]]
+	
+if GetConVar("scgg_enabled"):GetInt() >= 2 then
+	-- v Give the other variant to people that own one of them. (Notice: This only gives the scgg due to disappointing bugs)
+	for _,foundply in pairs(player.GetAll()) do
+		if foundply.SCGG_Dropping != true then
+		
+		for _,wep in pairs( foundply:GetWeapons() ) do
+			
+			if foundply:Alive() and wep:GetClass() == phys_string then
+				if !foundply:HasWeapon(superphys_string) then
+				foundply:Give(superphys_string)
+				end
+			--[[elseif foundply:Alive() and wep:GetClass() == superphys_string then
+				if !foundply:HasWeapon(phys_string) then
+				foundply:Give(phys_string)
+				end--]]
+			end
+		end
+		
+		end
+	end
+end
+	-- ^ End of above for loops.
+end
+
+if (game.GetGlobalState( "super_phys_gun") == GLOBAL_OFF) and GetConVar("scgg_allow_enablecvar_modify"):GetInt() > 0 then
+-- ^ Check if global state turned off and cvar is not 0
+	GetConVar("scgg_enabled"):SetInt(0)
+end
+
+end) 
+-- ^ End of think hook.
+
+--[[hook.Add("PlayerDeath","SCGG_Weapon_Drop_OnDeath",function( ply ) -- Acts like Keep Corpses :\
+if ply:HasWeapon(superphys_string) then
+	print("yeah")
+	local phys = ply:GetWeapon(superphys_string)
+	if phys.TP and phys.TP:IsValid() then
+		--if phys.HP then
+		
+		--end
+	ply:GetWeapon(superphys_string):Drop()
+	end
+end
+end)--]]
+
+--[[hook.Add("Think","SCGG_Weapon_ServerRagdoll_Think",function() -- Acts like Keep Corpses :\
+if GetConVar("scgg_enabled"):GetInt() >= 2 then
+	for _,npc in pairs(ents.GetAll()) do
+		if npc:IsNPC() then
+		if npc:GetShouldServerRagdoll() != true then
+			npc:SetShouldServerRagdoll( true )
+		end
+		end
+	end
+end
+end)--]]
+
+--[[hook.Add("PlayerCanPickupWeapon","SCGG_PreventWeaponPickup",function(ply, wep)
+	if GetConVar("scgg_enabled"):GetInt() >= 2 then
+		return false
+	end
+end)--]]
+
+cvars.AddChangeCallback( "physcannon_mega_enabled", function( convar_name, value_old, value_new )
+	-- ^ Support for physcannon_mega_enabled cvar.
+	local megacvar = tonumber(value_new)
+	if megacvar > 0 then
+		GetConVar("scgg_enabled"):SetInt(2)
+	end
+	if megacvar <= 0 then
+		GetConVar("scgg_enabled"):SetInt(0)
+	end
+end, "SCGG_MegaCvar_Support" )
+
+cvars.AddChangeCallback( "scgg_enabled", function( convar_name, value_old, value_new )
+-- ^ Checks for scgg enabled cvar change.
+	local enablecvar = tonumber(value_new)
+	if enablecvar >= 2 then
+		if Has1ChangedModifyCvar == true then
+		GetConVar("scgg_allow_enablecvar_modify"):SetInt(1)
+		Has1ChangedModifyCvar = false
+		end
+		game.SetGlobalState( "super_phys_gun", GLOBAL_ON )
+		if GetConVar("scgg_weapon_vaporize"):GetInt() <= 0 then
+		GetConVar("scgg_weapon_vaporize"):SetInt(1)
+		end
+	end
+	if (enablecvar < 2 and enablecvar > 0) then
+		--game.SetGlobalState( "super_phys_gun", GLOBAL_ON )
+		if GetConVar("scgg_allow_enablecvar_modify"):GetInt() > 0 then
+		GetConVar("scgg_allow_enablecvar_modify"):SetInt(0)
+		Has1ChangedModifyCvar = true
+		end
+		if GetConVar("scgg_weapon_vaporize"):GetInt() > 0 then
+		GetConVar("scgg_weapon_vaporize"):SetInt(0)
+		end
+	end
+	if enablecvar <= 0 then
+		if Has1ChangedModifyCvar == true then
+		GetConVar("scgg_allow_enablecvar_modify"):SetInt(1)
+		Has1ChangedModifyCvar = false
+		end
+		game.SetGlobalState( "super_phys_gun", GLOBAL_OFF )
+		if GetConVar("scgg_weapon_vaporize"):GetInt() > 0 then
+		GetConVar("scgg_weapon_vaporize"):SetInt(0)
+		end
+		
+		for _,ply in pairs(player.GetAll()) do
+		-- ^ Armor drainage.
+			local getcvar = GetConVar("scgg_keep_armor"):GetInt()
+			if getcvar <= 1 and ply:IsValid() and ply:Alive() and ply:Armor() >= 1 then
+			-- ^ Won't run of
+				local armorval_0 = ply:Armor()+1
+				local armor_countdown = ply:Armor()
+				local armor_reference = ply:Armor() -- Used to prevent added armor being removed. Although, doesn't work very well...
+				if (getcvar > 0 and getcvar < 2) and armor_reference <= 100 then return end
+				timer.Create( "SCGG_Armor_Lower", 0.01, armorval_0/2, function()
+					if (armor_reference % 2 == 0) then
+					armor_countdown = armor_countdown-2
+					else
+					armor_countdown = armor_countdown-1
+					end
+					if (getcvar <= 0 or getcvar >= 2) and ply:Armor() <= 0 -- If cvar is not just 1 (safety measure) and armor is 0...
+					or (getcvar > 0 and getcvar < 2) and ply:Armor() <= 100 -- or cvar is 1 and armor is 100...
+					then 
+					timer.Remove("SCGG_Armor_Lower") -- Stop draining.
+					return 
+					end
+					if ply:Armor() < armor_reference then
+						armor_reference = ply:Armor()
+					end
+					ply:SetArmor( armor_countdown+(ply:Armor()-armor_reference) ) -- Set the armor.
+				end )
+			end
+		end
+		-- ^ Armor drainage end.
+	end
+end, "SCGG_Disable_GlobalState" )
+-- ^ More like 'global state checking'
+-- v 
+cvars.AddChangeCallback( "scgg_allow_enablecvar_modify", function( convar_name, value_old, value_new )
+	if tonumber(value_new) > 0 then
+		Has1ChangedModifyCvar = false
+	end
+end, "SCGG_MegaCvar_Support" )
